4a64ca7256e74d39c5eec5d264c5b9344f444780
yc-user <yc-user@company.com>
2023-07-16 10:12:02 +0000

    asda

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,15 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.178.186', '127.0.0.1', 'localhost', 'taski14sp.ddns.net']
 
 
 # Application definition
@@ -103,8 +105,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
 
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -113,3 +116,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
+
+sentry_sdk.init(
+        # В этой переменной будет значение для вашего проекта.
+    dsn="https://379bf3360bdf43dc80980fbc22cfde7b@o4505409773305856.ingest.sentry.io/4505409775927296",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    traces_sample_rate=1.0,
+
+    send_default_pii=True
+) 
